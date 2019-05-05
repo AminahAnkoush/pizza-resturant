99c2f15d8e250b55d0b79f925721964aa83e870f
BISAN <bissanadj@gmail.com>
2019-05-05 22:25:11 +0300

    Bisan-Commit

--- app/src/main/java/com/birzeit/pizzarestaurant/MainActivity.java
+++ app/src/main/java/com/birzeit/pizzarestaurant/MainActivity.java
@@ -7,11 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -25,43 +27,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button getStartedBtn = (Button) findViewById(R.id.btnGetStarted);
-        List<JSONObject> pizzaList = new ArrayList<JSONObject>();
 
-        getStartedBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                AsyncTask asyncTask = new AsyncTask() {
-//
-//                    //@SuppressLint("WrongThread")
-//                    @Override
-//                    protected Object doInBackground(Object[] objects) {
-//                        OkHttpClient client = new OkHttpClient();
-//
-//                        Request request = new Request.Builder()
-//                                .url("http://www.mocky.io/v2/5b522fa32e000074005c1c40")
-//                                .build();
-//                        Response response = null;
-//                        try{
-//                            response = client.newCall(request).execute();
-//                            return response.body().string();
-//                        }catch (IOException e){
-//                            e.printStackTrace();
-//                        }
-//
-//                        return null;
-//                    }
-//
-//                    @Override
-//                    protected void onPostExecute(Object o) {
-//                        super.onPostExecute(o);
-//                    }
-//                }.execute();
-
-
-//                Intent getStartedToRegistrationIntent=new Intent(MainActivity.this,
-//                        CustomerMain.class);
-
-            }
-        });
     }
 }
